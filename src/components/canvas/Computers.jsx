120f0727593e284e901ce39ad494704970a3c9d5
anubhabguha1999 <anubhabguha1999@gmai.com>
2025-06-13 19:44:17 +0530

    Refactor Hero, Works, and Computers components for mobile responsiveness and iPhone detection; update project descriptions and images in constants.

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -9,7 +9,7 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
+      <hemisphereLight intensity={0.15} groundColor="black" />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
@@ -31,31 +31,33 @@ const Computers = ({ isMobile }) => {
 
 const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
+  const [isIPhone, setIsIPhone] = useState(false);
 
   useEffect(() => {
-    // Add a listener for changes to the screen size
     const mediaQuery = window.matchMedia("(max-width: 768px)");
-
-    // Set the initial value of the `isMobile` state variable
     setIsMobile(mediaQuery.matches);
 
-    // Define a callback function to handle changes to the media query
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
     };
 
-    // Add the callback function as a listener for changes to the media query
     mediaQuery.addEventListener("change", handleMediaQueryChange);
 
-    // Remove the listener when the component is unmounted
+    // Detect if the device is an iPhone
+    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    const isIphoneDetected = /iPhone/.test(userAgent);
+    setIsIPhone(isIphoneDetected);
+
     return () => {
       mediaQuery.removeEventListener("change", handleMediaQueryChange);
     };
   }, []);
-if (isMobile) return null
+
+  // If it's a mobile and NOT an iPhone, hide the canvas
+  if (isMobile && !isIPhone) return null;
   return (
     <Canvas
-      frameloop='demand'
+      frameloop="demand"
       shadows
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
